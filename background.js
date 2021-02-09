63cbbba00a69e1a9d7135424eb70cdafefdfa929
huangyt05 <huang.yutao.colin@gmail.com>
2021-02-09 03:20:44 -0500

    1

---
+++ background.js
@@ -0,0 +1,28 @@
+var canvas;
+let model1;
+
+function preload() {
+    model1 = loadModel('model.obj');
+}
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    canvas.style('z-index', '-1');
+}
+
+function draw() {
+    let dirX = (mouseX / width - 0.5) * 2;
+    let dirY = (mouseY / height - 0.5) * 2;
+    background(0);
+    orbitControl();
+    push();
+    ambientLight(100);
+    noStroke();
+    scale(9);
+    directionalLight(80, 80, 80, -dirX, -dirY, 1);
+    pointLight(50, 50, 50, mouseX, mouseY, 2);
+    specularMaterial(20);
+    shininess(8);
+    model(model1);
+    
+}
